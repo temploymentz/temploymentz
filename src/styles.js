1788ee3e925252534334f69eed65561fe4c9cf59
Vansh Rajput <135845531+vanshrajput099@users.noreply.github.com>
2025-11-09 21:25:28 +0530

    Initial commit from Create Next App

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+export const style = {
+    header:{
+        fontSize: 'text-4xl',
+        fontWeight: 'font-bold',
+    },
+    para:{
+        fontSize: 'text-base',
+        lineHeight: 'leading-6',
+    }
+}
